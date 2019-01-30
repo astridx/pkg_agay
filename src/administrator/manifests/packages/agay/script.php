fbcbcecedd73b957bef4eb07e988f69e8f708ea7
astridx <info@astrid-guenther.de>
2019-01-30 11:26:52 +0100

    wip

--- src/administrator/manifests/packages/agay/script.php
+++ src/administrator/manifests/packages/agay/script.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Site
- * @subpackage  pkg_aggpxtrack
+ * @subpackage  pkg_agay
  *
  * @copyright   Copyright (C) 2005 - 2018 Astrid Günther, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later;
